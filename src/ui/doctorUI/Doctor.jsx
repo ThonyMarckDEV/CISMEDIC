c8db4a9628b54f5e8ed04da1f248256994a309a9
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-02-05 20:54:17 -0500

    gg

--- src/ui/doctorUI/Doctor.jsx
+++ src/ui/doctorUI/Doctor.jsx
@@ -29,7 +29,7 @@ const Doctor = () => {
                   Bienvenido, {nombreUsuario || "Usuario"}
                 </h1>
                 <p className="text-violet-100 text-lg">
-                 Estamos aquí para cuidarte.
+                 Contamos con sus increibles servicios.
                 </p>
               </div>
               <div className="absolute right-0 top-0 w-1/3 h-full opacity-10">
@@ -50,7 +50,7 @@ const Doctor = () => {
               <div className="h-16 w-16 flex items-center justify-center rounded-full bg-green-100">
                 <Calendar className="h-8 w-8 text-green-600" />
               </div>
-              <h2 className="text-xl font-semibold">Todavía no tienes citas.</h2>
+              <h2 className="text-xl font-semibold">Citas Reservadas.</h2>
               <button className="inline-flex items-center justify-center px-4 py-2 rounded-md font-medium bg-green-600 text-white hover:bg-green-700 focus:outline-none">
                 <Link to="/doctor/miscitas">Ver mis Citas</Link> {/* Usa Link para la navegación */}
               </button>
