e1086f74a7e01049d2550d4a4abae87ec2ae72da
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-02-23 10:52:59 -0500

    gg

--- src/ui/adminUI/Admin.jsx
+++ src/ui/adminUI/Admin.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useEffect, useState } from "react"
-import { NotebookTextIcon, FileText, ChevronRight } from "lucide-react"
+import { NotebookTextIcon, FileText, ChevronRight , UserPenIcon } from "lucide-react"
 import SidebarAdmin from "../../components/adminComponents/SidebarAdmin"
 import jwtUtils from "../../utilities/jwtUtils"
 import { Link } from "react-router-dom"; // Importa Link desde react-router-dom
@@ -61,7 +61,7 @@ const Admin = () => {
              <div className="rounded-lg shadow-md bg-white p-6">
             <div className="flex flex-col items-center text-center gap-4">
               <div className="h-16 w-16 flex items-center justify-center rounded-full bg-green-100">
-                <NotebookTextIcon className="h-8 w-8 text-green-600" />
+                <UserPenIcon className="h-8 w-8 text-green-600" />
               </div>
               <p className="text-gray-700">¿Te gustaría agendar una cita a un cliente?</p>
               <button className="inline-flex items-center justify-center px-4 py-2 rounded-md font-medium bg-green-600 text-white hover:bg-green-700 focus:outline-none">
