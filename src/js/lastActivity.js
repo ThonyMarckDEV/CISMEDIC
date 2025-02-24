f872a2e3119500161582c63c6c10627c9a830bcc
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-02-24 00:04:15 -0500

    gg

--- src/js/lastActivity.js
+++ src/js/lastActivity.js
@@ -13,6 +13,42 @@ export async function updateLastActivity() {
         const token = jwtUtils.getTokenFromCookie();
         const userId = jwtUtils.getIdUsuario(token);
 
+        // Verificar si hay una sesión activa en otro dispositivo
+        const responseCheck = await fetch(`${API_BASE_URL}/api/check-active-session`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ idUsuario: userId })
+        });
+
+        if (!responseCheck.ok) {
+            throw new Error(`Error en check-active-session: ${responseCheck.status}`);
+        }
+
+        const { activeSession } = await responseCheck.json();
+
+        if (activeSession) {
+            // Si hay una sesión activa en otro dispositivo, cerrar la sesión actual
+            await fetch(`${API_BASE_URL}/api/logout`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({ idUsuario: userId })
+            });
+
+            // Eliminar el token de localStorage
+            jwtUtils.removeTokenFromCookie();
+
+            // Redirigir a la página de inicio de sesión en el dominio raíz
+            window.location.href = `/`;
+            return;
+        }
+
+        // Actualizar la última actividad
         const response = await fetch(`${API_BASE_URL}/api/update-activity`, {
             method: 'POST',
             headers: {
@@ -27,7 +63,5 @@ export async function updateLastActivity() {
         }
     } catch (error) {
         console.error('Error en updateLastActivity:', error);
-    }finally{
-        checkUserStatus();
     }
 }
