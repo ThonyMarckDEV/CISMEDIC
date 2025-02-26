bf0dfbceced1e4fd1b5faeea59a764ca3e7dd662
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-02-26 08:11:03 -0500

    gg

--- src/js/urlHelper.js
+++ src/js/urlHelper.js
@@ -1,8 +1,8 @@
 // src/js/urlHelper.js
 
-//const API_BASE_URL = 'https://cismedictunnel.thonymarckdev.online';
+const API_BASE_URL = 'https://cismedictunnel.thonymarckdev.online';
 
-const API_BASE_URL = 'http://localhost:8000';
+//const API_BASE_URL = 'http://localhost:8000';
 
 
 export default API_BASE_URL
