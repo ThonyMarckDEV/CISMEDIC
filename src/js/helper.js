53547307ad60eff5c0d6ba8570e3a5099d2ad88e
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-03-03 08:18:32 -0500

    gg

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,5 +1,5 @@
-// src/js/Helper.js
+// // src/js/Helper.js
 
-const API_TOKEN ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRob255bWFyY2szODUyMTN4ZEBnbWFpbC5jb20ifQ.i2LQzVYFJwQ0uRFQDDuGvsgqNlfNXeEH8-pm_GbUhqQ'
+// const API_TOKEN ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRob255bWFyY2szODUyMTN4ZEBnbWFpbC5jb20ifQ.i2LQzVYFJwQ0uRFQDDuGvsgqNlfNXeEH8-pm_GbUhqQ'
 
-export default API_TOKEN
+// export default API_TOKEN
